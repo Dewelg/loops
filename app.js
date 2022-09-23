cc12818c175c1be228d2c9919ac84bc86e4dc23c
_Dewelg_ <_(dewel713@gmail.com_)>
2022-09-22 23:33:41 -0400

    Progress keep going

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
+//exercise1
 let i = 0;
 
 while(i <= 100){
+    if (i % 2 == 0){
+        console.log(i);
+    }
+}
+
+for (i = 0; i < 100; i++){
     if (i % 2){
         console.log(i);
     }
 }
 
-
-
-/*for (i = 0; i < 100; i++){
-    if (i % 2){
-        console.log(i);
-    }
-}
-
-
-/*let i = 0
+//exercise2
+let i = 0
 do{
     i = i + 1
     console.log(i);
@@ -36,10 +35,10 @@ for (i = 0; i < 100; i++){
     
     } 
 } 
-
+//excserise3
 let i = 1;
 
-/*while(i <= 100){
+while(i <= 100){
     let output = '';
 
     if (i % 3 == 0){
@@ -69,4 +68,21 @@ do{
     console.log(`${i} ${output}`);
 
     i++
-} while(i <= 100) */
+} while(i <= 100) 
+
+//exercise4
+let value = Math.round(Math.random() * 500); // creates a random number between 0 and 500
+let n = Math.round(Math.random() * (500 - 100) + 100); // creates a random number between 100 and 500
+
+for (i = 0; i < n; i++){
+    console.log(i);
+    if (i == value){
+        console.log("Found value!");
+    
+        break;
+    } else if(i != value){
+        console.log("Did not find value");
+
+    }
+    
+} 
