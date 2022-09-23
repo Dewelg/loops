78f9d168217ee84d4c9a5b0362aaa79eee61cf4a
_Dewelg_ <_(dewel713@gmail.com_)>
2022-09-22 22:53:01 -0400

    improving

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+let i = 0;
+
+while(i <= 100){
+    if (i % 2){
+        console.log(i);
+    }
+}
+
+
+
 /*for (i = 0; i < 100; i++){
     if (i % 2){
         console.log(i);
@@ -5,10 +15,58 @@
 }
 
 
-let i = 0
+/*let i = 0
 do{
     i = i + 1
     console.log(i);
-} while(i < 100)*/
+} while(i < 100)
 
+for (i = 0; i < 100; i++){
+    console.log(i);
+    if(i % 3 && i % 5 == 0){
+        console.log('FIZZBUZZ');
+    }else{
+    
+        if (i % 3 == 0){
+            console.log('Fizz');
+        } 
+        if (i % 5 == 0){
+                console.log('BUZZ'); 
+        } 
+    
+    } 
+} 
 
+let i = 1;
+
+/*while(i <= 100){
+    let output = '';
+
+    if (i % 3 == 0){
+        output += 'FIZZ';
+    }
+
+    if (i % 5 == 0){
+        output += 'BUZZ';
+    }
+
+    console.log(`${i} ${output}`);
+
+    i++
+} 
+
+do{
+    let output = '';
+
+    if (i % 3 == 0){
+        output += 'FIZZ';
+    }
+
+    if (i % 5 == 0){
+        output += 'BUZZ';
+    }
+
+    console.log(`${i} ${output}`);
+
+    i++
+} while(i <= 100) */
